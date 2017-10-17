9b174b9aa79f99f9b59158eee0f56e79b577aa60
jisung0920 <jisung0920@gmail.com>
2017-10-17 20:11:12 +0900

    init

--- app/src/main/java/com/example/jisung/samecode/MainActivity.java
+++ app/src/main/java/com/example/jisung/samecode/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.jisung.samecode;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    public void onClick(View v){
-        Intent intent = new Intent(this,Code_ListActivity.class);
-        startActivity(intent);
-    }
+//    public void onClick(View v){
+//        Intent intent;
+//        switch (v.getId()){
+//            case R.id.travel:
+//                intent = new Intent(this,travelActivity.class);
+//                break;
+//            case R.id.music:
+//                intent = new Intent(this,musicActivity.class);
+//                break;
+//            case R.id.date:
+//                intent = new Intent(this,dateActivity.class);
+//                break;
+//            case R.id.cook:
+//                intent = new Intent(this,cookActivity.class);
+//                break;
+//            case R.id.book:
+//                intent = new Intent(this,bookActivity.class);
+//                break;
+//            case R.id.movie:
+//                intent = new Intent(this,movieActivity.class);
+//                break;
+//            case R.id.menu:
+//                intent = new Intent(this,travelActivity.class);
+//                break;
+//            default:
+//                intent = new Intent(this,travelActivity.class);
+//                break;
+//        }
+//        startActivity(intent);
+//
+//    }
 }
